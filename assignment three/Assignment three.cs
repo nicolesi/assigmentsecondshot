b503c76f100f03976121aa90269aabb0a8e4a3e3
nicolesi <39331658+nicolesi@users.noreply.github.com>
2018-11-14 18:42:49 +1300

    Add files via upload

---
+++ assignment three/Assignment three.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignmentThree1
+{
+    public partial class ShowAllForms : StudentsPreformaceForm
+    {
+        public ShowAllForms()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Close everything down.
+            Application.Exit();
+        }
+
+        private void button2_MouseClick(object sender, MouseEventArgs e)
+        {
+            new StudentsPreformaceForm().Show();
+            this.Hide();
+        }
+
+       
+    }
+}
